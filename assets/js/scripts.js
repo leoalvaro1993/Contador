81a59e0a77335016e210d38a4348fd5a5d625c76
Leonardo Alvaro de Souza <leo-alvaro@hotmail.com>
2022-07-14 11:36:06 -0300

    Contador
    Aula prática da DIO

---
+++ assets/js/scripts.js
@@ -0,0 +1,38 @@
+window.addEventListener("load", () => {
+    const botao = document.getElementById("botao");
+
+    botao.addEventListener("click", () => {
+        
+        let sec = 360;
+
+        const countDiv = document.getElementById("timer");
+
+        const secpass = () => {
+            
+            let min = Math.floor(sec/60);
+            let segundosRestantes = sec % 60;
+
+
+            if (segundosRestantes < 10) {
+                segundosRestantes = "0" + segundosRestantes;
+            }
+
+            if (min <10 ) {
+                min = "0" + min;
+            }
+
+            countDiv.innerHTML = min + ":" + segundosRestantes;
+
+            if (sec >0) {
+                sec = sec - 1;
+            }
+            else {
+                countDiv.innerHTML = "Tempo Esgotado";
+            }
+
+        }
+    
+    const countDown = setInterval(() => secpass(), 1000);
+
+    });
+})
